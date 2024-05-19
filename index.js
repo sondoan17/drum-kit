c2c200d4276448eda8abdc6bdce4486728f91b10
sondoan17 <sondoan17@gmail.com>
2024-05-20 04:11:34 +0700

    fix playDrumSoundKeyboard

--- index.js
+++ index.js
@@ -15,37 +15,37 @@ function playDrumSound(event) {
   var drumSound = new Audio("./sounds/" + soundFileName[drumIndex] + ".mp3");
   drumSound.play();
 }
-
+function playDrumSoundKeyboard(event){
+    keyPress = event.key; // Update the value of keyPress
+    switch (keyPress) {
+      case "w":
+        var drumSound = new Audio("./sounds/crash.mp3");
+        drumSound.play();
+        break;
+      case "a":
+        var drumSound = new Audio("./sounds/kick-bass.mp3");
+        drumSound.play();
+        break;
+      case "s":
+        var drumSound = new Audio("./sounds/snare.mp3");
+        drumSound.play();
+        break;
+      case "d":var drumSound = new Audio("./sounds/tom-1.mp3");
+        drumSound.play();
+        break;
+      case "j":var drumSound = new Audio("./sounds/tom-2.mp3");
+        drumSound.play();
+        break;
+      case "k":var drumSound = new Audio("./sounds/tom-3.mp3");
+        drumSound.play();
+        break;
+      case "l":var drumSound = new Audio("./sounds/tom-4.mp3");
+        drumSound.play();
+        break;
+    }
+}
 var drums = document.querySelectorAll(".drum");
 drums.forEach(function (drum) {
   drum.addEventListener("click", playDrumSound);
 });
-document.addEventListener("keyup", function (event) {
-  keyPress = event.key; // Update the value of keyPress
-  switch (keyPress) {
-    case "w":
-      var drumSound = new Audio("./sounds/crash.mp3");
-      drumSound.play();
-      break;
-    case "a":
-      var drumSound = new Audio("./sounds/kick-bass.mp3");
-      drumSound.play();
-      break;
-    case "s":
-      var drumSound = new Audio("./sounds/snare.mp3");
-      drumSound.play();
-      break;
-    case "d":var drumSound = new Audio("./sounds/tom-1.mp3");
-      drumSound.play();
-      break;
-    case "j":var drumSound = new Audio("./sounds/tom-2.mp3");
-      drumSound.play();
-      break;
-    case "k":var drumSound = new Audio("./sounds/tom-3.mp3");
-      drumSound.play();
-      break;
-    case "l":var drumSound = new Audio("./sounds/tom-4.mp3");
-      drumSound.play();
-      break;
-  }
-});
+document.addEventListener("keyup", playDrumSoundKeyboard);
